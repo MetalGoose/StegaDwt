987d52c8df441ba52226573b0ee5b2dabc80d901
Евгений Беляков <freelans73@gmail.com>
2022-05-19 11:58:12 +0300

    Added "Encode" page

---
+++ StegaDwt.UI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using StegaDwt.UI.Core;
+
+namespace StegaDwt.UI.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public EncodeViewModel EncodeVM { get; set; }
+
+        public MainViewModel()
+        {
+            EncodeVM = new EncodeViewModel();
+            CurrentView = EncodeVM;
+        }
+    }
+}
